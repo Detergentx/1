0e78e9ddf6e89317b3a0c77d3eef1994f8aa94ec
laundryDetergent <77954331+Detergentx@users.noreply.github.com>
2022-06-24 20:24:11 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="masonry">
+    <div class="item" :key="i.id" v-for="i in data">
+      <img :src="i.img">
+      <span class="title">{{i.value}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import data from "./data.json";
+export default {
+  data() {
+    return {
+      data
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.masonry {
+  column-count: 3;
+  column-gap: 10px;
+  padding: 10px;
+  .item {
+    break-inside: avoid;
+    border: 1px solid #999;
+    margin-bottom: 10px;
+    img {
+      width: 100%;
+      vertical-align: middle;
+    }
+    .title {
+      display: block;
+      margin-left: 5px;
+    }
+  }
+}
+</style>
